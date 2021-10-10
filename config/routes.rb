99875359d726a9abba9e1e003039d3a6cd64c7c0
Johan Hanses <johanhanses@gmail.com>
2021-10-10 09:03:36 +0200

    remove new users path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users#, skip: :registrations
+  devise_for :users, skip: :registrations
 
   get "/cv", to: "cv#index"
   get "/about", to: "about#index"
